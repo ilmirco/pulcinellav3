04da0004bc1e685f7c399debecc1ee0fb20232b3
ilmirco <mir23mir54@gmail.com>
2026-06-05 12:49:12 +0200

    Add files via upload

--- create_icon.py
+++ create_icon.py
@@ -1,6 +1,6 @@
 ﻿from PIL import Image, ImageDraw, ImageFont
 size = 180
-img = Image.new('RGB', (size, size), (17, 17, 17))
+img = Image.new('RGB', (size, size), (255, 255, 255))
 draw = ImageDraw.Draw(img)
 ring_width = 10
 bounding_box = [15, 15, size-15, size-15]
@@ -15,11 +15,11 @@ except:
         font = ImageFont.load_default()
 
 try:
-    draw.text((size/2, size/2 - 5), 'P', fill=(250, 250, 245), font=font, anchor='mm')
+    draw.text((size/2, size/2 - 5), 'P', fill=(17, 17, 17), font=font, anchor='mm')
 except TypeError:
     bbox = draw.textbbox((0,0), 'P', font=font)
     w = bbox[2] - bbox[0]
     h = bbox[3] - bbox[1]
-    draw.text((size/2 - w/2, size/2 - h/2 - 15), 'P', fill=(250, 250, 245), font=font)
+    draw.text((size/2 - w/2, size/2 - h/2 - 15), 'P', fill=(17, 17, 17), font=font)
 
 img.save('apple-touch-icon.png')
